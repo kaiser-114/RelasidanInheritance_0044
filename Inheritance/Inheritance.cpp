f985898a702469336b86851bc3c461bdff3960ae
kaiser-114 <areezilla@gmail.com>
2026-05-11 10:51:25 +0700

    add orang and pelajar classes with constructors and methods for introduction

---
+++ Inheritance/Inheritance.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class orang
+{
+public:
+    string nama;
+
+    orang(string pNama) : nama(pNama)
+    {
+        cout << "orang dibuat\n"
+             << endl;
+    }
+    ~orang()
+    {
+        cout << "orang dihapus\n"
+             << endl;
+    }
+    int jumlah(int a, int b)
+    {
+        return a + b;
+    }
+};
+
+class pelajar : public orang
+{
+public:
+    string sekolah;
+
+    pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah)
+    {
+    }
+    ~pelajar()
+    {
+        cout << "pelajar dihapus \n"
+             << endl;
+    }
+    string perkenalan(){
+        return "Hello, nama saya " + nama + " dari sekolah" + "\n\n"}
+};
+
