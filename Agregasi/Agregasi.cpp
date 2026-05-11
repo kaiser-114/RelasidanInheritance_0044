1dd850f669f32947ee7bd07d16f641bdee1666e5
kaiser-114 <areezilla@gmail.com>
2026-05-11 10:40:57 +0700

    implement main function in Agregasi.cpp to create instances of ibu and anak

--- Agregasi/Agregasi.cpp
+++ Agregasi/Agregasi.cpp
@@ -6,4 +6,10 @@ using namespace std;
 #include "ibu.h"
 
 int main()
-{}
+{
+    ibu *varIbu = new ibu("dini");
+    ibu *varIbu2 = new ibu("novi");
+    anak *varAnak1 = new anak("tono");
+    anak *varAnak2 = new anak("rini");
+    anak *varAnak3 = new anak("dewi");
+}
