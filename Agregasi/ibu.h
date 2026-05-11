d1440681fee49a671486f37ef91dee2ab3ff9ae1
kaiser-114 <areezilla@gmail.com>
2026-05-11 10:44:17 +0700

    add class definition for ibu with constructor and destructor

--- Agregasi/ibu.h
+++ Agregasi/ibu.h
@@ -1,2 +1,21 @@
 #ifndef IBU_H
 #define IBU_H
+
+class ibu
+{
+public:
+    srting nama;
+    vector<anak *> daftar_anak;
+
+    ibu(string pNama) : nama(pNama)
+    {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+
+    ~ibu()
+    {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
+    void tambahAnak(anak*);
+    void cetakAnak();
+};
