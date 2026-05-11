05a3f29262f002539168f15bc2405aa80bc60ab0
kaiser-114 <areezilla@gmail.com>
2026-05-11 10:51:38 +0700

    add main function to demonstrate pelajar class functionality

--- Inheritance/Inheritance.cpp
+++ Inheritance/Inheritance.cpp
@@ -40,3 +40,12 @@ public:
         return "Hello, nama saya " + nama + " dari sekolah" + "\n\n"}
 };
 
+int main()
+{
+    pelajar siswa1("andi laksono", "SMAN 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+    return 0;
+    ;
+}
